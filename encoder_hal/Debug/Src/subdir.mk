8049761c2a660e31120d182dffeb41ebfeed8550
michal.rom089 <michal.rom089@gmail.com>
2018-03-07 13:11:42 +0100

    minimal working example

---
+++ encoder_hal/Debug/Src/subdir.mk
@@ -0,0 +1,46 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/encoder_lib.c \
+../Src/gpio.c \
+../Src/main.c \
+../Src/spi.c \
+../Src/stm32f4xx_hal_msp.c \
+../Src/stm32f4xx_it.c \
+../Src/system_stm32f4xx.c \
+../Src/usart.c 
+
+OBJS += \
+./Src/encoder_lib.o \
+./Src/gpio.o \
+./Src/main.o \
+./Src/spi.o \
+./Src/stm32f4xx_hal_msp.o \
+./Src/stm32f4xx_it.o \
+./Src/system_stm32f4xx.o \
+./Src/usart.o 
+
+C_DEPS += \
+./Src/encoder_lib.d \
+./Src/gpio.d \
+./Src/main.d \
+./Src/spi.d \
+./Src/stm32f4xx_hal_msp.d \
+./Src/stm32f4xx_it.d \
+./Src/system_stm32f4xx.d \
+./Src/usart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 '-D__weak=__attribute__((weak))' '-D__packed="__attribute__((__packed__))"' -DUSE_HAL_DRIVER -DSTM32F411xE -I"/home/mromanow/Dokumenty/RobotFirmware/encoder_hal/Inc" -I"/home/mromanow/Dokumenty/RobotFirmware/encoder_hal/Drivers/STM32F4xx_HAL_Driver/Inc" -I"/home/mromanow/Dokumenty/RobotFirmware/encoder_hal/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy" -I"/home/mromanow/Dokumenty/RobotFirmware/encoder_hal/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"/home/mromanow/Dokumenty/RobotFirmware/encoder_hal/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
