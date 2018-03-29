d640435a47a184d30d9b081b6dc728559c6d9f3d
michal.rom089 <michal.rom089@gmail.com>
2018-03-29 16:35:15 +0200

    task_manager init

---
+++ robot_main/Inc/task_manager.h
@@ -0,0 +1,29 @@
+/*
+ * task_manager.h
+ *
+ *  Created on: 29.03.2018
+ *      Author: mromanow
+ */
+
+#ifndef TASK_MANAGER_H_
+#define TASK_MANAGER_H_
+
+#define TASK_BUFFER_SIZE 2
+
+struct task{
+	int   task_type;
+	float time;
+
+	float joint0;
+	float joint1;
+	float joint2;
+};
+
+typedef struct task task;
+
+task buffer[TASK_BUFFER_SIZE];
+task *actual_task;
+
+
+
+#endif /* TASK_MANAGER_H_ */
---
+++ robot_main/Src/task_manager.c
@@ -0,0 +1,7 @@
+/*
+ * task_manager.c
+ *
+ *  Created on: 29.03.2018
+ *      Author: mromanow
+ */
+
