9bd5104dabbc2868866120baf1a23f722c2c9a03
michal.rom089 <michal.rom089@gmail.com>
2018-03-29 16:35:35 +0200

    communication module init

---
+++ robot_main/Inc/communication.h
@@ -0,0 +1,13 @@
+/*
+ * communication.h
+ *
+ *  Created on: 29.03.2018
+ *      Author: mromanow
+ */
+
+#ifndef COMMUNICATION_H_
+#define COMMUNICATION_H_
+
+
+
+#endif /* COMMUNICATION_H_ */
---
+++ robot_main/Src/communication.c
@@ -0,0 +1,16 @@
+/*
+ * communication.c
+ *
+ *  Created on: 29.03.2018
+ *      Author: mromanow
+ */
+
+#include "communication.h"
+
+void sendMsg(int msg){
+
+}
+void msgReceivedCallback(){
+
+}
+
