6f9c8ac80792b7705468c8c0b514ae63d2037657
michal.rom089 <michal.rom089@gmail.com>
2018-03-29 18:08:17 +0200

    Doxygen stepper+motion_contorl headers

--- robot_main/Inc/motion_control.h
+++ robot_main/Inc/motion_control.h
@@ -12,45 +12,103 @@
 #include "main.h"
 #include "limits.h"
 
-#define EPSILON	0.05
+#define EPSILON	0.05			/**< defines the accuracy of reached position*/
 #define ACCEL_DISTANCE 0.5
 
+/** @file motion_control.h
+ *  @brief Function prototypes and public variables for motion controll of the stepper motors.
+ *
+ *  This contains all functions and variables necessary for proper motion control including velocity,acceleration and position limits (of the joints).
+ *  The m_control() and m_updatePositionAll() functions are executed periodically by TIM3 PeriodElapsedCallback event.
+ */
+
+/**
+ * A structure to represent motion control node.
+ */
 struct motion_node{
 
-	float 		  max_position;
-	float 		  min_position;
+	float 		  max_position;		/**< maximum position that can reached */
+	float 		  min_position;		/**< minimum position that can reached */
 
-	uint32_t	  max_speed;	//maximum speed limit of the joint
-	uint32_t	  min_speed;	//minimum speed limit of the joint
-	float 		  max_accel;	//maximum acceleration limit of the joint
+	uint32_t	  max_speed;		/**< maximum speed limit of the joint */
+	uint32_t	  min_speed;		/**< minimum speed limit of the joint */
+	float 		  max_accel;		/**< maximum acceleration limit of the joint */
 
-	float 		  actual_position;	//position of the joint
-	uint32_t	  actual_speed;		//actual speed of the joint
-	float 		  goal_position;	//goal position of the joint
-	uint32_t	  goal_speed;		//goal speed (maximum speed while moving towards goal position)
+	float 		  actual_position;	/**< actual position of the joint */
+	uint32_t	  actual_speed;		/**< actual speed of the joint */
+	float 		  goal_position;	/**< goal position of the joint */
+	uint32_t	  goal_speed;		/**< goal speed (maximum speed while moving towards goal position)*/
 
-	int   		  enabled;
+	uint8_t 	  enabled;			/**< determines wheter the motion control node is disabled(0) or enabled(1) */
 
 };
 
 typedef struct motion_node motion_node;
 
-motion_node motion_nodes[STEPPER_N];
+motion_node motion_nodes[STEPPER_N]; /**< array of motion node that are related to stepper motors used by the microcontroller */
 
+/**
+ * Initialization function.
+ * Sets all required variables that are related to the motion controll node.
+ * @see \struct motion_node
+ */
 void m_motionControllerInit();
 
+/**
+ * Periodically executed function. This function oversees all joints (its velocity, acceleration and position)
+ * @see \struct motion_node
+ */
 void m_control();
-
-void m_setPosition(uint8_t motor,float position,uint32_t speed);
-
+/**
+ * This function sets the goal_position to which the joint is going to.
+ * @param motor determines the stepper motor (actually the motion node that are related to the motor)
+ * @param position determines the position to which the joint is going to
+ * @param time	determines the time of the travel
+ * @see \struct motion_node
+ * @return Void
+ */
+void m_setPosition(uint8_t motor,float position,uint32_t time);
+/**
+ * This function updates the actual_position of the joint
+ * @param motor determines the stepper motor (actually the motion node that are related to the motor)
+ * @see \struct motion_node
+ * @return Void
+ */
 void m_updatePosition(uint8_t motor);
+/**
+ * This function updates the actual_position of all joints
+ * @see \struct motion_node
+ * @return Void
+ */
 void m_updateAllPosition();
 
 int m_calculateSpeed(uint8_t motor,uint32_t speed);
 
+/**
+ * This function enables the motion control node.
+ * @param motor determines the stepper motor (actually the motion node that are related to the motor)
+ * @see \struct motion_node
+ * @return Void
+ */
 void m_enable(uint8_t motor);
+/**
+ * This function enables all motion control node.
+ * @see \struct motion_node
+ * @return Void
+ */
 void m_enableAll();
+/**
+ * This function disables the motion control node.
+ * @param motor determines the stepper motor (actually the motion node that are related to the motor)
+ * @see \struct motion_node
+ * @return Void
+ */
 void m_disable(uint8_t motor);
+/**
+ * This function disables all motion control node.
+ * @see \struct motion_node
+ * @return Void
+ */
 void m_disableAll();
 
 #endif /* MOTION_CONTROL_H_ */
--- robot_main/Inc/stepper_lib.h
+++ robot_main/Inc/stepper_lib.h
@@ -11,45 +11,107 @@
 #include "stdint.h"
 #include "main.h"
 #include "limits.h"
-//
-//	Lib params configured for TIM f=100kHz
-//
-//
 
 
+#define STEPPER_N	3 /**< defines the number of stepper motors that are used*/
+
+/** @file stepper_lib.h
+ *  @brief Function prototypes and public variables for stepper motor control
+ *
+ *  This contains all functions and variables necessary for proper stpper motor control including velocity limits and motor specification (physical
+ *  connections from MICRO-CONTROLLER to MOTOR, micro-step info etc.) The s_step() or s_stepAll() functions are executed periodically by TIM2 PeriodElapsedCallback event.
+ */
+
+/**
+ * A structure to represent stepper motor
+ */
 struct stepper{
-	uint16_t			step_pin;
-	GPIO_TypeDef 		*step_port;
-	uint16_t			dir_pin;
-	GPIO_TypeDef 		*dir_port;
-	uint8_t		 		step;	// 1/2 -> 2 | 1/4 -> 4 ...
+	uint16_t			step_pin;		/**< pin of micro-controller that is connected to the driver step input*/
+	GPIO_TypeDef 		*step_port;		/**< port of micro-controller that is connected to the driver step input*/
+	uint16_t			dir_pin;		/**< pin of micro-controller that is connected to the driver direction input*/
+	GPIO_TypeDef 		*dir_port;		/**< port of micro-controller that is connected to the driver direction input*/
+	uint8_t		 		step;	  		/**< micro-step of the stepper motor (eg. step=2 means that the stepper motor runs with 1/2 micro-step)*/
 
 	//limits
-	uint32_t			max_speed;
-	uint32_t			min_speed;
-	uint8_t				clockwise_direction; //position is going towards pi (if dir=!clockwise_direction position is going toward -pi)
+	uint32_t			max_speed;		/**< maximum speed of the stepper motor (related to the physical limitations of the stepper motor)*/
+	uint32_t			min_speed;		/**< minimum speed of the stepper motor (related to the physical limitations of the stepper motor)*/
+	uint8_t				direction; 		/**< determines direction of the rotation (if direction=!1 position is going toward -pi)*/
 
-	uint32_t			timer_period;
-	uint32_t			timer_counter;
+	uint32_t			timer_period;   /**< maximum value to which timer_counter is counting up */
+	uint32_t			timer_counter;  /**< if timer_counter==timer_period the toggle on the step pin occurs*/
 
-	uint8_t				enabled;
+	uint8_t				enabled;		/**< determines whether the stepper motor is enabled or disabled*/
 };
 typedef struct stepper stepper;
 
-stepper motors[STEPPER_N];
+stepper motors[STEPPER_N]; /**< array of stepper motors that are used by the microcontroller */
 int steps=0;
 
+
+/**
+ * Initialization function.
+ * Sets all required variables that are related to the stepper motor
+ * @see \struct stepper
+ */
 void s_motorsInit();
 
+/**
+ * This function simply counts up timer_counter of the stepper motor. If timer_counter=timer_period the toggle on the step pin occurs.
+ * @param motor determines the stepper motor
+ * @see \struct stepper
+ * @return Void
+ */
 void s_step(uint8_t motor);
+/**
+ * Periodically executed function.This function counts up timer_counter of all stepper motors. If timer_counter=timer_period the toggle on the step pin occurs.
+ * @see \struct stepper
+ * @see s_step()
+ * @return Void
+ */
 void s_stepAll();
 
-void s_changeDir(uint8_t motor,uint8_t dir); //dir==1 clockwise dir==0 counter_clockwise
+/**
+ * This function changes the direction of rotation of the stepper motor.
+ * @param motor determines the stepper motor
+ * @param dir desired direction of the rotation
+ * @see \struct stepper
+ * @return Void
+ */
+void s_changeDir(uint8_t motor,uint8_t dir);
+/**
+ * This function changes the speed of rotation of the stepper motor.
+ * @param motor determines the stepper motor
+ * @param speed desired speed of the rotation
+ * @see \struct stepper
+ * @return Void
+ */
 void s_setSpeed(uint8_t motor,uint32_t speed);
 
+/**
+ * This function enables the stepper motor.
+ * @param motor determines the stepper motor
+ * @see \struct stepper
+ * @return Void
+ */
 void s_enable(uint8_t motor);
+/**
+ * This function enables of disables all of stepper motors.
+ * @see \struct stepper
+ * @return Void
+ */
 void s_enableAll();
+/**
+ * This function disables the stepper motor.
+ * @param motor determines the stepper motor
+ * @see \struct stepper
+ * @return Void
+ */
 void s_disable(uint8_t motor);
+/**
+ * This function enables of disables all of stepper motors.
+ * @see \struct stepper
+ * @return Void
+ */
 void s_disableAll();
 
 #endif /* STEPPER_LIB_H_ */
